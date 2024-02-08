51266c9b1d8f685d26cb449b6dcea977feba7243
benjaAguilar <benjaaguilar02@gmail.com>
2024-02-08 11:35:09 -0300

    started the function check game
    this function check for a three in a row or tie
    (not finished yet)

--- js/script.js
+++ js/script.js
@@ -54,6 +54,39 @@ let game = (function(){
         }
     }
 
+    //Checks if there are a 3 in a row or a tie
+    function checkGameboard(){
+
+        for(let i = 0; i <= 2; i++){
+            //row search
+            gameFlow.gameBoard[i].forEach((row) => {
+                console.log(row)
+            });
+            
+            //column search
+            gameFlow.gameBoard.forEach((column) => {
+                console.log(column[i]);
+            });
+
+            //diagonal search
+            gameFlow.gameBoard.forEach((diagonal) => {
+                if(indexOf(diagonal) === 0){
+                    console.log(diagonal[0]);
+
+                } else if(indexOf(diagonal === 1)){
+                    console.log(diagonal[1]);
+
+                } else{
+                    console.log(diagonal[2]);
+                    
+                }
+            });
+
+            
+        }
+        
+    }
+
     function addMark(x, y){
         let markValue = gameFlow.gameBoard[y][x][0];
 
@@ -62,6 +95,7 @@ let game = (function(){
 
             console.table(gameFlow.gameBoard);
             toggleTurn();
+            checkGameboard();
 
         } else{
             console.error("That place its already taken");
@@ -73,7 +107,7 @@ let game = (function(){
     gameFlow.playerOne = addPlayer("playerOne");
     gameFlow.playerTwo = addPlayer("playerTwo");
     toggleTurn();
-
+    
     return{
         addMark: addMark,
     }
