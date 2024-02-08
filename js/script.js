8e4eb89e2fc81dce6439afbc9ea8b7f54bb28409
benjaAguilar <benjaaguilar02@gmail.com>
2024-02-08 10:09:44 -0300

    Created a module pattern with some functions
    this has an object that have the gameflow
    factory function to create the player
    function to toggle the players turn

---
+++ js/script.js
@@ -0,0 +1,67 @@
+let game = (function(){
+
+    let playerTurn;
+
+    let gameFlow = {
+        gameBoard: [[[0], [0], [0]],
+                    [[0], [0], [0]],
+                    [[0], [0], [0]]],
+        playerOne: {},
+        playerTwo: {},
+    }
+
+    //Create the players
+    function addPlayer(defaultName){
+        let name = prompt("whats your name?", defaultName);
+        let mark;
+        let hasPlayed = false;
+
+        if(gameFlow.playerOne.mark === undefined){
+            mark = prompt("choose a mark! 1 = X, 2 = O");
+            mark === 2 ? mark = "O" : mark = "X";
+
+        } else if(gameFlow.playerOne.mark === "X"){
+            mark = "O";
+
+        } else{
+            mark = "X";
+
+        }
+        
+        return{
+            name,
+            mark,
+            hasPlayed
+        }
+    }
+    
+    function toggleTurn(){
+
+        if(gameFlow.playerOne.hasPlayed === false){
+            playerTurn = gameFlow.playerOne;
+
+            gameFlow.playerOne.hasPlayed = true;
+            gameFlow.playerTwo.hasPlayed = false;
+            console.warn(gameFlow.playerOne.name + " Turn!");
+
+        } else{
+            playerTurn = gameFlow.playerTwo;
+
+            gameFlow.playerOne.hasPlayed = false;
+            gameFlow.playerTwo.hasPlayed = true;
+            console.warn(gameFlow.playerTwo.name + " Turn!");
+
+        }
+    }
+
+    function addMark(x, y){
+        console.log(playerTurn.mark);
+
+    }
+
+    gameFlow.playerOne = addPlayer("playerOne");
+    gameFlow.playerTwo = addPlayer("playerTwo");
+    toggleTurn();
+    addMark();
+
+})();
